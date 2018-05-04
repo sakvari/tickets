ad827ea6d55c4118995614e9f767d8ea11e756f1
sakvari <magana.arnoldo@gmail.com>
2018-05-04 18:39:16 -0500

    add mysql connection

--- main_tickets.py
+++ main_tickets.py
@@ -1,13 +1,30 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
- 
+from flask.ext.mysql import MySQL
+
+# it's neccesary install "pip install flask-mysql"
 # App config.
 DEBUG = True
 app = Flask(__name__)
+#mysql connection
+mysql=MySQL()
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='123456'
+app.config['MYSQL_DATABASE_DB']='ticket_et'
+app.config['MYSQL_DATABASE_host']='127.0.0.1:3306'
+mysql.init_app(app)
+conn = mysql.connect() # let's create the MySQL connection:
+cursor = conn.cursor() # let's create cursor 
+
+#end mysql cnnection
+
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
- 
+
+
+
 class ReusableForm(Form):
+    
     name = TextField('Name:', validators=[validators.required()])
     extension = TextField('Extension:', validators=[validators.required()])
     email = TextField('Email:', validators=[validators.required()])
@@ -27,11 +44,18 @@ def hello():
  
         if form.validate():
             # Save the comment here.
-            flash('Thanks for registration ' + name)
+            flash('Listo, Estamos listos para empezar a trabajar... ' + name)
         else:
-            flash('Error: All the form fields are required. ')
+            flash('Error: Llena bien los datos por favor')
  
     return render_template('hello.html', form=form)
- 
+
+@app.route("/MySQL")
+def test_mysql():
+    try:
+        c, conn = connection()
+        return("okay")
+    except Exception as e:
+        return(str(e))
 if __name__ == "__main__":
     app.run()
