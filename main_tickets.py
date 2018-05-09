bcc887f262a0afcbda015fe2e1aeaacf386cc663
sakvari <magana.arnoldo@gmail.com>
2018-05-09 18:57:05 -0500

    Add login route

--- main_tickets.py
+++ main_tickets.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, Response
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from flask.ext.mysql import MySQL
 
 # it's neccesary install "pip install flask-mysql"
 # App config.
-#this it's the first version from table  i'll be change it  create table ticket (id_ticket INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40) NOT NULL, email VARCHAR(50) NOT NULL, extension VARCHAR(8), description TEXT,criticality VARCHAR(7) ,red_date TIMESTAMP);
+# pip install flask-login
+# I had  create a database called ticket_et
+# after i had create this table: create table ticket (id_ticket INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40) NOT NULL, email VARCHAR(50) NOT NULL, extension VARCHAR(8), description TEXT,criticality VARCHAR(7) ,red_date TIMESTAMP, red_date_end TIMESTAMP, assignto VARCHAR(25), status VARCHAR(15) DEFAULT 'open');
+# this it's the  users table
+# create table users (id_ticket INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,name VARCHAR(35),username VARCHAR(25),password VARCHAR(35), email VARCHAR(40))
+# insert INTO users (name,username,password,email) VALUES ("arnoldo magana","maganaa","nocturna","maganaa@e-telecomm.com.mx")
+# 
+# 
 DEBUG = True
 app = Flask(__name__)
 #mysql connection
@@ -25,13 +32,16 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 class ReusableForm(Form):
-    
     name = TextField('Name:', validators=[validators.required()])
     extension = TextField('Extension:', validators=[validators.required()])
     email = TextField('Email:', validators=[validators.required()]) 
     comp_select = TextField('comp_select:', validators=[validators.required()]) 
     report = TextField('Report:', validators=[validators.required(), validators.Length(min=2, max=35)])
- 
+    
+class ReusableFormLogin(Form):
+    user = TextField('User:', validators=[validators.required()])
+    password = TextField('Password:', validators=[validators.required()])
+    
 @app.route("/", methods=['GET', 'POST'])
 def add_ticket():
     form = ReusableForm(request.form)
@@ -56,6 +66,19 @@ def add_ticket():
  
     return render_template('add_ticket.html', form=form, data=[{'criticality':'Normal'}, {'criticality':'Baja'}, {'criticality':'Alta'}])
 
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = ReusableFormLogin(request.form)
+    if request.method == 'POST':
+        user=request.form['user']
+        password=request.form['password']
+        if form.validate():
+            print("hola")
+        
+        else:
+            flash('Error: Llena bien los datos por favor...')
+    return render_template('login.html', form=form)
 @app.route("/MySQL")
 def test_mysql():
     try:
